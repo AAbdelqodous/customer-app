c7d3f58cc13b5b8b1aa6e029100e712bd1fd79b1
AAbdelqodous <ml.ahmed1187@gmail.com>
2024-07-16 18:11:55 +0300

    aop complete

--- src/main/java/com/elite/customer_app/CustomerAppApplication.java
+++ src/main/java/com/elite/customer_app/CustomerAppApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.elite.customer_app")
 @EntityScan(basePackages = "com.elite.customer_app.model")
 @EnableJpaRepositories(basePackages = "com.elite.customer_app.repository")
-//@EnableAspectJAutoProxy
+@EnableAspectJAutoProxy
 public class CustomerAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/elite/customer_app/aspect/LoggingAspect.java
+++ src/main/java/com/elite/customer_app/aspect/LoggingAspect.java
@@ -1,41 +1,104 @@
 package com.elite.customer_app.aspect;
 
-import org.aopalliance.intercept.Joinpoint;
+import com.elite.customer_app.model.Customer;
+import org.aspectj.lang.*;
+import org.aspectj.lang.annotation.*;
 import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
-//@Aspect
+import java.util.List;
+import java.util.logging.Logger;
+
+@Aspect
 @Order(2)
+@Component
 public class LoggingAspect {
+    Logger logger = Logger.getLogger(getClass().getName());
 
-    // @Before("execution( * com.elite.customer_app.controller.*.saveCustomer(..)")
-    public void beforeAddCustomerAdvice(Joinpoint joinpoint){
+    @Before("execution(* com.elite.customer_app.controller.*.saveCustomer(..))")
+    public void beforeAddCustomerAdvice(JoinPoint joinpoint){
+        String methodSignature = joinpoint.getSignature().toShortString();
+        logger.info("---------> Method signature: " + methodSignature);
 
-        /* MethodSignature methodSignature = (MethodSignature) joinpoint.getMethodSignature();
-        System.out.println("---------> Method signature: " + methodSignature); */
-
-        /* Object[] args =joinpoint.getArgs();
+        Object[] args = joinpoint.getArgs();
         for(Object arg : args) {
-            System.out.println("---------> Argument: " + arg);
+            logger.info("---------> Argument: " + arg);
 
-            if(arg instance of Customer){
+            if(arg instanceof Customer){
                 Customer customer = (Customer) arg;
-                System.out.println( customer.getFirstName());
-                System.out.println( customer.getLastName());
-                System.out.println( customer.getEmail());
+                logger.info(customer.getFirstName());
+                logger.info(customer.getLastName());
+                logger.info(customer.getEmail());
             }
-        } */
+        }
 
-        System.out.println("---------> Executing @Before advice on saveCustomer() <--------");
+        logger.info("---------> Executing @Before advice on saveCustomer() <--------");
     }
 
-    //    @Before("com.elite.customer_app.aspect.AopUtil.repoPointCut()")
+    @Before("com.elite.customer_app.aspect.AopUtil.repoPointCut()")
     public void beforeRepoAdvice(){
-        System.out.println("---------> Executing @Before advice on method <---------");
+        logger.info("---------> Executing @Before advice on method <---------");
     }
 
-    //    @Before("com.elite.customer_app.aspect.AopUtil.repoAdviceExceptGetOneAndGetByIdPointcut()")
+    @Before("com.elite.customer_app.aspect.AopUtil.repoAdviceExceptGetOneAndGetByIdPointcut()")
     public void beforeRepoAdviceExceptGetOneAndGetById(){
-        System.out.println("---------> Executing @Before Logging Aspect on method <---------");
+        logger.info("---------> Executing @Before Logging Aspect on method <---------");
     }
 
+    @AfterReturning(
+            pointcut = "execution(* com.elite.customer_app.repository.CustomerRepository.findAll(..))",
+            returning = "customers"
+    )
+    public void afterFindAllCustomersAdvice(JoinPoint joinpoint, List<Customer> customers){
+        String method = joinpoint.getSignature().toShortString();
+        logger.info("----------> @AfterReturning on method: " + method);
+
+        convertLastNameToUpper(customers);
+
+        logger.info("----------> End of @AfterReturning on method: " + method);
+    }
+
+    private void convertLastNameToUpper(List<Customer> customers) {
+        customers.forEach(customer -> customer.setLastName(customer.getLastName().toUpperCase()));
+    }
+
+    @AfterThrowing(
+            pointcut = "execution(* com.elite.customer_app.repository.CustomerRepository.findAll(..))",
+            throwing = "exception"
+    )
+    public void afterThrowingFindAllCustomersAdvice(JoinPoint joinpoint, Throwable exception){
+        String method = joinpoint.getSignature().toShortString();
+        logger.info("----------> @AfterThrowing on method: " + method);
+        logger.info("----------> The exception is: " + exception);
+    }
+
+    @After("execution(* com.elite.customer_app.service.CustomerService.findAll(..))")
+    public void afterFindAllCustomerAdvice(JoinPoint joinpoint){
+        String method = joinpoint.getSignature().toShortString();
+        logger.info("----------> @After (Finally) on method: " + method);
+    }
+
+    @Around("execution(* com.elite.customer_app.repository.CustomerRepository.delete(..))")
+    public Object aroundDeleteCustomer(ProceedingJoinPoint proceedingJointPoint) throws Throwable {
+        String method = proceedingJointPoint.getSignature().toShortString();
+        logger.info("----------> @Around on method: " + method);
+
+        long begin = System.currentTimeMillis();
+
+        Object result = null;
+
+        try {
+            result = proceedingJointPoint.proceed();
+        } catch (Exception e) {
+            logger.warning(e.getMessage());
+            result = "Major accident, but do not worry!";
+        }
+
+        long end = System.currentTimeMillis();
+        long duration = end - begin;
+
+        logger.info("----------> It takes : " + duration + " milliseconds");
+
+        return result;
+    }
 }
